01360795db958d1713aeb4c41ad9aa5de505b0e4
George Mylonas <mylonasg88@gmail.com>
2020-07-25 18:02:48 +0800

    Deleted forgotten postgres files

--- dc-postgres/NO-docker-entrypoint-initdb.d/init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Add extra user to the database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker;
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
-EOSQL
